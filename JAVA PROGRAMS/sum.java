5cb635360f6feb0079e4fa05cfa2565ce5d48faf
Rishabh Arya <rishabharya810@gmail.com>
2025-12-23 17:29:49 +0530

    feat: Add sum.java file

---
+++ JAVA PROGRAMS/sum.java
@@ -0,0 +1,14 @@
+public class sum 
+{
+    public static void main(String[] args) 
+    {
+        int a=10;
+        int b=22;
+        int sum =a+b;
+        int diff=b-a;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println("Hello World");
+    
+    }  
+}
