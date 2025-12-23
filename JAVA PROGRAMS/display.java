51c3dc30b1dec0936a81183cbe2e2b5cf68b0053
Rishabh Arya <rishabharya810@gmail.com>
2025-12-23 18:29:51 +0530

    feat: Add display.java file

---
+++ JAVA PROGRAMS/display.java
@@ -0,0 +1,23 @@
+//package com.apnacollege;
+
+
+import java.util.Scanner;
+
+public class display
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	
+	    System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+		{
+			String name = sc.next();
+			System.out.println(name);
+
+		}
+       
+		{
+			sc.close();
+		}
+	}
+}
