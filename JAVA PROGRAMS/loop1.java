461fd6d2d4bcbf05c5d382ede9b643071da9413e
Rishabh Arya <rishabharya810@gmail.com>
2025-12-24 09:40:28 +0530

    feat: Add loop1.java for basic loop example

---
+++ JAVA PROGRAMS/loop1.java
@@ -0,0 +1,9 @@
+public class loop1 {
+    public static void main(String args[])
+    {
+        for(int counter = 0;counter<100;counter++)
+        {
+            System.out.println("namaste");
+        }
+    }    
+}
