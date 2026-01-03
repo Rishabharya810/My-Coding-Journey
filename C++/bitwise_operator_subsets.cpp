ba258fe7f67ae465b2c8b1951e63ad0d50192c5f
Rishabh Arya <rishabharya810@gmail.com>
2026-01-03 12:24:05 +0530

    Implement bitwise subset generation.

---
+++ C++/bitwise_operator_subsets.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void subsets(int arr[],int n)
+{
+    cout<<(1<<n)<<endl;
+
+    for(int i=0;i<(1<<n);i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i & (1<<j))
+            {
+                cout<<arr[j]<<" ";
+            }
+        }
+        cout<<endl;
+    } 
+    
+}
+
+int main()
+{
+    
+    int arr[4]={1,2,3,4};
+    subsets(arr,4);
+
+    return 0;
+}
