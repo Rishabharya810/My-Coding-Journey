63be9cc0f398edcefcac685897d096ae90971814
Rishabh Arya <rishabharya810@gmail.com>
2026-01-22 09:59:39 +0530

    Add file.cpp

---
+++ C++/file.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // long string a,b,c,d,e,f,g,h,i,j,k,l;
+    // cin>>a;
+    // cin>>b;
+    // cin>>c;
+    // cin>>d;
+    // cin>>e;
+    // cin>>f;
+    // cin>>g;
+    // cin>>h;
+    char a [80];
+    char b [80];
+    char c [80];
+    char d [80];
+    char e [80];
+    char f [80];
+    char g [80];
+    char h [80];
+    char i [80];
+    char j [80];
+
+
+
+    cin.getline(a,100);
+    cin.getline(b,100);
+    cin.getline(c,100);
+    cin.getline(d,100);
+    cin.getline(e,100);
+    cin.getline(f,100);
+    cin.getline(g,100);
+    cin.getline(h,100);
+    cin.getline(i,100);
+    cin.getline(j,100);
+   
+
+    return 0;
+}
+
