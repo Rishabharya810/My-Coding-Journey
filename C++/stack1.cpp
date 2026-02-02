af96f5051d2f4a38d2cd8d54207f88f3eafed94a
Rishabh Arya <rishabharya810@gmail.com>
2026-02-02 09:00:40 +0530

    Add stack1.cpp

---
+++ C++/stack1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string str2;
+    getline(cin,str2);
+    cout<<str2<<endl;
+
+    string str;
+    cin>>str;
+    cout<<str<<endl;
+    string str1(5,'n');
+    cout<<str1<<endl;
+    
+
+    return 0;
+}
