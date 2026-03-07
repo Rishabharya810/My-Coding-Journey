113a6a3fbd3130da979f54a7485ba09bc4066099
Rishabh Arya <rishabharya810@gmail.com>
2026-03-07 11:14:37 +0530

    Added push_and_pop.js file demonstrating array push and pop operations.

---
+++ WEB DEVELOPMENT/JAVASCRIPT/push_and_pop.js
@@ -0,0 +1,11 @@
+let fruits = ['Apple', 'Banana'];
+
+length = fruits.length;    // Get initial length i.e. 2
+console.log(fruits);   // Output: ['Apple', 'Banana']
+console.log(length);   // Output: 2 
+
+fruits.push('Orange');    // Add 'Orange' to the end
+newLength = fruits.length;  // Get new length i.e. 3
+
+console.log(fruits);     // Output: ['Apple', 'Banana', 'Orange']
+console.log(newLength);  // Output: 3
