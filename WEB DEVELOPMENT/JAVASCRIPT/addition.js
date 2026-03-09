02ec798a7dc79d106f202b57e968db2afb4a2a5b
Rishabh Arya <rishabharya810@gmail.com>
2026-03-09 10:09:41 +0530

    Added addition.js file with JavaScript addition examples.

---
+++ WEB DEVELOPMENT/JAVASCRIPT/addition.js
@@ -0,0 +1,9 @@
+console.log(4+5); 
+console.log("10"+ 20);
+console.log("100" + "200");
+console.log(50 + "50");
+console.log("hey" + (5 + 4));
+// console.log((5 + 4) + "hey");
+// console.log("hey" + 5 + 4);
+console.log(5 + 4 + "hey"); 
+console.log("hey" + 5 + 4); 
