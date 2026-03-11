047d10f243e79ea9d9dc9b52cce2610119b9ac7e
Rishabh Arya <rishabharya810@gmail.com>
2026-03-11 12:00:09 +0530

    Implemented slice functionality.

---
+++ WEB DEVELOPMENT/JAVASCRIPT/Slice.js
@@ -0,0 +1,10 @@
+let fruits = ['Apple', 'Banana', 'Orange', 'Mango', 'Grape'];
+
+// Copy from index 1 up to (but not including) index 3
+let someFruits = fruits.slice(1, 3);
+
+console.log(someFruits); 
+// Output: ['Banana', 'Orange']
+
+console.log(fruits);     
+// Output: ['Apple', 'Banana', 'Orange', 'Mango', 'Grape'] (Unchanged!)
