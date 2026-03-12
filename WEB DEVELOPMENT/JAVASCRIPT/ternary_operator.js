874fea5bd274b53c21475cba7b4a35baafd8066f
Rishabh Arya <rishabharya810@gmail.com>
2026-03-12 09:55:07 +0530

    Added ternary_operator.js example demonstrating conditional logic.

---
+++ WEB DEVELOPMENT/JAVASCRIPT/ternary_operator.js
@@ -0,0 +1,11 @@
+red_light = "Red";
+// red_light = "Yellow";
+// red_light = "Green";
+
+if (red_light === "Red") {
+    console.log("Stop");
+} else if (red_light === "Green") {
+    console.log("Go");
+} else if (red_light === "Yellow") {
+    console.log("Caution");
+}
